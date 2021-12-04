6292d20acca95c9d4da2bb2a36e8c32e4df56070
Simon Que <simon_justin_que@dlsu.edu.ph>
2021-12-04 14:22:31 +0800

    routes.py add index route

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,8 @@ from app import App
 from datetime import datetime, date
 import os
 import re
+
+@App.route('/', methods=['GET'])
+@App.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html')
