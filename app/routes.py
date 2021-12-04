c65a44685de2cd2a5a9e5441c2e4cc1d7fbd2549
Simon Que <simon_justin_que@dlsu.edu.ph>
2021-12-04 18:43:47 +0800

    app can now be run to display the test index page

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,22 @@
 from flask import render_template, url_for, request, jsonify, send_from_directory
+
 from app import App
 from datetime import datetime, date
 import os
 import re
 
+'''
+route for index page
+'''
 @App.route('/', methods=['GET'])
 @App.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@App.route('/forms/<formname>', methods['GET'])
+'''
+route for forms
+formname parameter is the short name of the form
+'''
+@App.route('/forms/<formname>', methods=['GET'])
 def renderForm(formname):
     return render_template(f"{formname}.html")
