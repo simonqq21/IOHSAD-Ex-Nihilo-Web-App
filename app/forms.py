721b264bbaf216da70b5130807acf857fdfef869
Simon Que <simon_justin_que@dlsu.edu.ph>
2021-12-05 18:25:45 +0800

    defined ComplaintForm in fields

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TelField, EmailField, TextAreaField
 from wtforms.validators import DataRequired
 
-class FormA(FlaskForm):
-    pass
+'''
+-Name of Company
+-Presence of union (If yes, put down the contact details of the head of the union (contact number and email)
+-Contact Details of the one who submitted the form
+
+then finally a large textbox for the nature of the complaint
+'''
+class ComplaintForm(FlaskForm):
+    companyName = StringField("Company Name", validators=[DataRequired()])
+    unionPresence = BooleanField()
+    unionHeadContactNo = TelField()
+    unionHeadEmail = EmailField()
+    contactNumber = TelField()
+    complaint = TextAreaField()
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
 from flask import render_template, url_for, request, jsonify, send_from_directory
 
 from app import App
+from app.forms import ComplaintForm
 from datetime import datetime, date
-import os
-import re
 
 '''
 route for index page
@@ -19,5 +18,5 @@ formname parameter is the short name of the form
 '''
 @App.route('/forms/<formname>', methods=['GET'])
 def renderForm(formname):
-    
+
     return render_template(f"{formname}.html")
