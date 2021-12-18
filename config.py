a7d8ad1e185f036f1073f44bbd2418e83b093a72
Simon Que <simon_justin_que@dlsu.edu.ph>
2021-12-18 17:11:45 +0800

    r

--- config.py
+++ config.py
@@ -6,8 +6,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "postgresql+psycopg2://simonque:12345678@localhost/swengdb"  # or other relevant config var
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-    # rest of connection code using the connection string `SQLALCHEMY_DATABASE_URI`
 
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require') or \
-    # "postgresql+psycopg2://simonque:12345678@localhost/swengdb"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
