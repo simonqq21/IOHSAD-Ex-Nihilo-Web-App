f8478b3297294894341208757bffe05e2f71992f
Simon Que <simon_justin_que@dlsu.edu.ph>
2022-01-28 20:22:48 +0800

    f

--- app/models.py
+++ app/models.py
@@ -442,3 +442,13 @@ addQuestionsToForm("Form A", ["complaint"])
 
 forma = selectForm('Form A')
 print(forma)
+
+masterAdmin = Administrator(username = 'IOHSAD', email = 'testemail')
+masterAdmin.set_password('iohsad2022')
+session.add(masterAdmin)
+commit()
+
+adm = aliased(Administrator, name='adm')
+masterAdmin = session.query(adm).where(or_(adm.username.like("IOHSAD"), adm.email.like('testemail'))).first()
+masterAdmin.email = '2newemail'
+commit()
